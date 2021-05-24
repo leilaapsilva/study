4b4a07c86b64285ab1959f75983fe80c3fe85623
leilaapsilva <leilaapsilva26@gmail.com>
2021-05-24 12:28:52 -0300

    Fix exercise

--- hackerrank/python/designer-door-mat.py
+++ hackerrank/python/designer-door-mat.py
@@ -5,6 +5,8 @@
 n, m = input().split()
 n, m = int(n), int(m)
 
+largura = m
+
 traco, ponto_barra_ponto = '-', '.|.'
 
 for i in range (int((n-1)/2)):
@@ -12,9 +14,12 @@ for i in range (int((n-1)/2)):
     m = m - 6
     print(tracos + ponto_barra_ponto*(i*2+1) + tracos)
         
-print(traco * n + 'WELCOME' + traco * n)
+print(traco * int((largura - 7)/2) + 'WELCOME' + traco * int((largura - 7)/2))
+
+# (m - 7)/2
 
 for j in reversed(range (int((n-1)/2))):
     m = m + 6
     tracos = traco*int((m-3)/2)
     print(tracos+ ponto_barra_ponto*(j*2+1)+ tracos)
+    
